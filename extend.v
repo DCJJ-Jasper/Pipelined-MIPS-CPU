038cedd4b5a8afc2d03189c982cffb2753e5b85e
Terence McHugh <tjm044@bucknell.edu>
2016-10-11 21:15:17 -0400

    adding tests

--- extend.v
+++ extend.v
@@ -4,3 +4,14 @@ module extend(input [15:0] immediate,output [31:0] extened);
 endmodule // extend
 
      
+module test();
+   wire [15:0]immed = 16'h 4241;
+   wire [31:0] extend;
+   
+    initial
+     $monitor("immed:%h extend:%h",immed,extend);
+   extend e(immed,extend);
+   
+endmodule // test
+
+   
