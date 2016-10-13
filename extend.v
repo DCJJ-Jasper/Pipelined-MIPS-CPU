9577c8c0a93da571873327b7ecff09376a062cf3
Terence McHugh <tjm044@bucknell.edu>
2016-10-13 10:59:21 -0400

    fixing tests

--- extend.v
+++ extend.v
@@ -1,15 +1,21 @@
 module extend(input [15:0] immediate,output [31:0] extened);
-   assign extened = immediate;
+   wire [31:0] all1 = 32'hffff0000;
+   
+   assign extened = (immediate[15]==0) ? immediate : immediate+all1;
 
 endmodule // extend
 
      
 module test();
-   wire [15:0]immed = 16'h 4241;
+   reg [15:0]immed = 16'h8241;
    wire [31:0] extend;
-   
+   initial begin
+      #10 immed = 16'h4241;
+      
+      
+   end
     initial
-     $monitor("immed:%h extend:%h",immed,extend);
+     $monitor("immed:%b extend:%b",immed,extend);
    extend e(immed,extend);
    
 endmodule // test
--- top.v
+++ top.v
@@ -52,4 +52,6 @@ module top(input wire clk);
    alu jumpALU(0,leftinstr32,pcPlus,jumpPC);//uses ALUOp of 0 for add
    //shiftleft2 sl(instruction[`target],); may have to implement? confused with wires merging top shift left 2
    mux muxBranch(pcPlus,jumpPC,Branch,branchPC)
-endmodule
+     endmodule // top
+
+
