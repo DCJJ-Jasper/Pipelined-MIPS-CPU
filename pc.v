4f94bec8c407e35bc59b92bf4705fe1355c42cc4
Ari Faust <amf027@bucknell.edu>
2016-10-16 15:55:08 -0400

    adding mips.h

--- pc.v
+++ pc.v
@@ -1,7 +1,7 @@
 module pc (input clk,input [31:0] nextPC,output [31:0] PC);
    reg [31:0] temp;
    initial
-     temp = 31'h0;//temporary start
+     temp = 31'h00100000;//temporary start
 
    assign #10 PC = temp;
    
