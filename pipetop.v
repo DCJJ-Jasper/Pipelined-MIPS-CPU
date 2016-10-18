ee64124940075ec64d1c885fef47a9226299c131
cd032 <cd032@bucknell.edu>
2016-10-18 17:28:55 -0400

    not done yet for pipetop.v

--- pipetop.v
+++ pipetop.v
@@ -1,3 +1,4 @@
+`timescale 1ns/1ns
 `include "mips.h"
 
 module pipetop; //(input wire clk)
@@ -44,15 +45,60 @@ module pipetop; //(input wire clk)
     wire [31:0] rega;
     wire [31:0]     aluresult;
 
+    // Here is the part for pipetop
+    wire clk;
+    wire StallD;
+    wire PCSrcD;
+
+    wire [31:0] InstrF;
+    wire [31:0] PCPlus4F;
+
+    wire [31:0] InstrD;
+    wire [31:0] PCPlus4D;
 
     wire [31:0] PCF;
-    wire [31:0] instruction;
+    wire [31:0] instrF;
+
+    wire clk;
+    wire FlushE;
+    wire RegWriteD;
+    wire MemtoRegD;
+    wire MemWriteD;
+    wire [2:0] ALUControlD;
+    wire ALUSrcD;
+    wire RegDstD;
+    wire [4:0] RsD;
+    wire [4:0] RtD;
+    wire wire[4:0] RdD;
+    wire [31:0] SignImmD;
+
+    wire RegWriteE;
+    wire MemtoRegE;
+    wire MemWriteE;
+    wire [2:0] ALUControlE;
+    wire ALUSrcE;
+    wire RegDstE;
+    wire [4:0] RsE;
+    wire [4:0] RtE;
+    wire [4:0] RdE;
+    wire [31:0] SignImmE;
+
+
 
     // Why does instruction module there are 2 inputs for instruction module
     // syscall?
 
     // According to the graph, there should be only one input and one output
-    instruction instructionMem(PCF, instruction);
+    instruction instructionMem(PCF, instrF);
+    instruction instructionMem(regv, rega, sys, PC[31:2], instruction);
+
+    // 
+    adder add4toPCF(PCF, PCPlus4F);
+    FtoD theFtoD(clk, StallD, PCSrcD, InstrF, PCPlus4F, InstrD, PCPlus4D);
+
+    register Registers(clk, rs, rt, writereg, data, regwrite, data1, data2, regv, rega);
+
+    DtoE theDtoE(clk, FlushE, RegWriteD, MemtoRegD, MemWriteD, ALUControlD, ALUSrcD, RegDstD, BranchD, RsD, RtD, RdD, SignImmD, RegWriteE, MemtoRegE, MemWriteE, ALUControlE, ALUSrcE, RegDstE, RsE, RtE, RdE, SignImmE); 
 
 
 
@@ -62,7 +108,7 @@ module pipetop; //(input wire clk)
 
 
 
-
+    // Here is the old part
 
    
     mux5 muxInstr(rt,rd,RegDst,writereg);
