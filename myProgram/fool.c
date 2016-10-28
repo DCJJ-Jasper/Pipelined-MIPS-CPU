686eb5c4c56b659344e9c1ecfc14237b85ef86c1
cd032 <cd032@bucknell.edu>
2016-10-27 23:59:47 -0400

    small change

--- myProgram/fool.c
+++ myProgram/fool.c
@@ -10,7 +10,7 @@ int fool(int a, int b)
 	else if (a & b)  // & the upper 1 bits : 8 & 4 = 1000 & 0100 = 0
 		return (a * b);
 	else if (a | b) 
-		return (a + b);
+		return (a + b);		
 	else 
 		return (a - b);
 
@@ -20,7 +20,7 @@ int fool(int a, int b)
 int main(void)
 {
     char s[128]= {0};
-    int f = fool(8,4);
+    int f = fool(5,4);
     puts("Result is:");
     puts(itoa(f));
     puts("\n");
