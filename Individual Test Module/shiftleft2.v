7cc7c2a735ae1c95379588b5fa8b7d417476ce58
cd032 <cd032@bucknell.edu>
2016-10-27 21:05:56 -0400

    keep some individual test module

---
+++ Individual Test Module/shiftleft2.v
@@ -0,0 +1,24 @@
+
+
+
+
+module shiftleft2(instr, newinstr);
+
+	input [25:0] instr;
+	output [27:0] newinstr;
+
+	assign newinstr = instr*4;
+
+endmodule // extend
+
+/*
+module test();
+   wire [31:0]instr = 32'h 86000031;
+   wire [31:0] newin;
+   
+    initial
+     $monitor("immed:%h extend:%h",instr,newin);
+  shiftleft2 s(instr,newin);
+   
+endmodule // test
+*/
